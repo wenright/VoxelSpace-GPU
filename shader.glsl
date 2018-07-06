6932b73ad81b6d18cbdbe5007a2066c2e323fdd3
Will Enright <wenright0@gmail.com>
2018-07-05 20:57:02 -0400

    Fix strange banding issue

--- shader.glsl
+++ shader.glsl
@@ -3,25 +3,22 @@ uniform Image colorMap;
 uniform float phi;
 uniform vec3 player;
 
-float heightScale = 10000.0;
-float horizon = 120.0;
-float drawDistance = 50.0;
+vec4 backgroundColor = vec4(0.7, 0.8, 0.9, 1.0);
+float heightScale = 16000.0;
+float horizon = 100.0;
+float drawDistance = 150.0;
 
 vec4 effect(vec4 loveColor, Image texture, vec2 texture_coords, vec2 screen_coords) {
-    // With rotation
     float sinPhi = sin(phi);
     float cosPhi = cos(phi);
     vec4 color = vec4(0, 0, 0, 0);
 
-    float dz = 0.1;
-    for (float z = 0; z < drawDistance; z += dz) {
+    float dz = 0.25;
+    for (float z = 1; z < drawDistance; z += dz) {
       vec2 leftPoint = vec2(-cosPhi * z - sinPhi * z + player.x,
           sinPhi * z - cosPhi * z + player.y);
       vec2 rightPoint =  vec2(cosPhi * z - sinPhi * z + player.x,
           -sinPhi * z - cosPhi * z + player.y);
-
-      // vec2 delta = vec2((rightPoint.x - leftPoint.x) / screenDimensions.x,
-      //     (rightPoint.y - leftPoint.y) / screenDimensions.x);
 
       float dx = (rightPoint.x - leftPoint.x) / love_ScreenSize.x;
       float dy = (rightPoint.y - leftPoint.y) / love_ScreenSize.x;
@@ -29,26 +26,24 @@ vec4 effect(vec4 loveColor, Image texture, vec2 texture_coords, vec2 screen_coor
       leftPoint.x += dx * screen_coords.x;
       leftPoint.y += dy * screen_coords.x;
 
+      leftPoint.x = floor(leftPoint.x);
+      leftPoint.y = floor(leftPoint.y);
+
       leftPoint /= 200.0;
 
       float drawHeight = (player.z - Texel(heightMap, leftPoint).r) / z * heightScale + horizon;
 
       if (drawHeight < screen_coords.y) {
         color = Texel(colorMap, leftPoint);
-        // return Texel(heightMap, leftPoint);
+
+        // Add fog
+        float amount = sqrt((drawDistance - z) / drawDistance);
+        color = ((amount * color) + (1 - amount) * backgroundColor);
+
         return color;
-
-
-        // return vec4(1.0, drawHeight / 1000, 0, 1.0);
-        // break;
       }
 
-      // return vec4(1.0, leftPoint.x / 100, 0, 1.0);
-
-
-      // return Texel(heightMap, mod(leftPoint, vec2(1024, 1024)));
-      dz += 0.001;
-      // dz *= 1.01;
+      dz *= 1.02;
     }
 
     return color;
